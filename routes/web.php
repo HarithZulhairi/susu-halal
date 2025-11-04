e373f130477dc409980e5ebff27aa9b8d1eb9321
HarithZulhairi <misterwhoopy@gmail.com>
2025-11-04 22:20:51 +0800

    Add Milk Record Page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+// Milk Prcoessing and Record Module
+
+Route::get('/nurse/milk-process', function () {
+    return view('milk processing and records/nurse_MilkProcess');
+})->name('milk.process');
+
