83221f7a03348542c9328965ec7c0ea84e6d1a54
egzyyy <ariffnorjihan@gmail.com>
2025-11-27 01:18:16 +0800

    relationship User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,9 +24,40 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    // Relationships
+
     public function HmmcAdmin()
     {
         return $this->hasOne(HmmcAdmin::class, 'user_id', 'id');
     }
 
+    public function Nurse()
+    {
+        return $this->hasOne(Nurse::class, 'user_id', 'id');
+    }
+
+    public function Doctor()
+    {
+        return $this->hasOne(Doctor::class, 'user_id', 'id');
+    }
+
+    public function LabTech()
+    {
+        return $this->hasOne(LabTech::class, 'user_id', 'id');
+    }
+
+    public function ShariahCommittee()
+    {
+        return $this->hasOne(ShariahCommittee::class, 'user_id', 'id');
+    }
+
+    public function ParentModel()
+    {
+        return $this->hasOne(ParentModel::class, 'user_id', 'id');
+    }
+
+    public function Donor()
+    {
+        return $this->hasOne(Donor::class, 'user_id', 'id');
+    }
 }
